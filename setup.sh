c23e560dbcbc57cee6c714ce180f7fd7d5e45eae
moshee <moshee@displaynone.us>
2015-06-13 22:35:28 -0700

    asdfasdfasd

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 for i in bash_profile gitconfig tmux.conf vim vimrc; do
-	ln -s .$i ~/.$i
+	ln -s $(pwd)/.$i ~/.$i
 done
 
 cd bin
 for i in *; do
-	ln -s $i /usr/local/bin/$i
+	sudo ln -s $(pwd)/$i /usr/local/bin/$i
 done
