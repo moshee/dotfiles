3d0ee54d45aa9bb67a906742894d48599a7014f2
moshee <moshee@displaynone.us>
2016-08-03 10:15:39 -0700

    cygwin

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 for i in bash_profile gitconfig tmux.conf vim vimrc gitignore; do
 	if [ ! -e ~/.$i ]; then
-		ln -s $(pwd)/.$i ~/.$i
+		ln -fs "$(pwd)/.$i" ~/.$i
 	fi
 done
+
+SUDO=
+case $(uname | tr A-Z a-z) in
+	*cygwin*) ;;
+	*) SUDO=sudo ;;
+esac
 
 cd bin
 for i in *; do
 	if [ ! -e /usr/local/bin/$i ]; then
-		sudo ln -s $(pwd)/$i /usr/local/bin/$i
+		$SUDO ln -fs "$(pwd)/$i" /usr/local/bin/$i
 	fi
 done
 
